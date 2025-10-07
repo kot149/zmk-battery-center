fb838bf45e417a964b529f096c51912297c13072
kot149 <131101078+kot149@users.noreply.github.com>
2025-10-07 11:50:29 +0900

    refactor: Remove unnecessary @ts-nocheck and @ts-expect-error

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
-// @ts-nocheck
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from '@tailwindcss/vite'
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
-// @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
