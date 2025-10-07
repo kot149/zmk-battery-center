1155ba0ae534d809a06bb273c1c2afb785453c48
kot149 <131101078+kot149@users.noreply.github.com>
2025-10-07 15:18:55 +0900

    feat: Add tauri_plugin_os

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -15,6 +15,7 @@ const LOG_LEVEL: log::LevelFilter = log::LevelFilter::Warn;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_os::init())
         .plugin(
             tauri_plugin_log::Builder::new()
                 .level(LOG_LEVEL)
